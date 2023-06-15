c7a8e631f3be8b4dfa340ae2c26dd0916cd9365b
salamczyk <60439180+salamczyk@users.noreply.github.com>
2023-06-14 20:31:16 -0500

    physics marbles

--- Assets/Code/sphereController.cs
+++ Assets/Code/sphereController.cs
@@ -1,44 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class sphereController : MonoBehaviour
 {
-    public float xOffset;
-    public float yOffset;
-    public float zOffset;
-    private Vector3 initPos;
-
-    private float xSpeed;
-    private float ySpeed;
-    private float zSpeed;
-
+    Rigidbody rb;
+    public Vector3 initForce;
     // Start is called before the first frame update
     void Start()
     {
-        initPos = transform.position;
-        
-        xSpeed = xOffset;
-        ySpeed = yOffset;
-        zSpeed = zOffset;
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(initForce);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 currentPos = transform.position;
-
-        currentPos.x += xSpeed * Time.deltaTime;
-        currentPos.y += ySpeed * Time.deltaTime;
-        currentPos.z += zSpeed * Time.deltaTime;
-        
-        transform.position = currentPos;
 
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (collision.gameObject.GetComponent<playerController>() != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
