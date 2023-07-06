5aafe1882419415d0b7fc084e19b736741a406d6
salamczyk <salamczyk@gmail.com>
2023-07-06 17:14:42 -0400

    Added camera and character rotation

--- Assets/Code/cameraController.cs
+++ Assets/Code/cameraController.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class cameraController : MonoBehaviour
 {
     public GameObject character;
-    private Vector3 initOffset;
+    public float cameraDistance;
+
+    private float initialRotation;
+    private float initialHeight;
     // Start is called before the first frame update
     void Start()
     {
-        initOffset = character.transform.position - this.transform.position;
+        initialHeight = transform.position.y;
+        initialRotation = transform.rotation.eulerAngles.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.position = character.transform.position - initOffset;
+        transform.position = new Vector3(character.transform.position.x + -1 * cameraDistance * Mathf.Sin(Mathf.Deg2Rad * character.transform.rotation.eulerAngles.y), initialHeight, character.transform.position.z + -1 * cameraDistance * Mathf.Cos(Mathf.Deg2Rad * character.transform.rotation.eulerAngles.y));
+        transform.rotation = Quaternion.Euler(initialRotation, 1 * character.transform.rotation.eulerAngles.y, 0);
     }
 }
